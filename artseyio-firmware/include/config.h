86dc77c8ccd1a99923c0de52171892887ce27b8c
Andrew Ellison <andrew.kevin.ellison@gmail.com>
2023-03-22 22:35:15 +0200

    Reverted pinout changes to continue 1.0 support.

--- artseyio-firmware/include/config.h
+++ artseyio-firmware/include/config.h
@@ -6,8 +6,8 @@
 
 // Pin definitions
 #define PIN_A P32
-#define PIN_R P33
-#define PIN_T P34
+#define PIN_R P14
+#define PIN_T P15
 #define PIN_S P16
 #define PIN_E P17
 #define PIN_Y P31
@@ -15,8 +15,8 @@
 #define PIN_O P11
 
 // USB device descriptor
-#define USB_VENDOR_ID       0x0000    // VID
-#define USB_PRODUCT_ID      0x0000    // PID
+#define USB_VENDOR_ID       0x0000    // VID (shared www.voti.nl)
+#define USB_PRODUCT_ID      0x0000    // PID (shared HID-keyboards)
 #define USB_DEVICE_VERSION  0x0100    // v1.0 (BCD-format)
 
 // USB configuration descriptor
